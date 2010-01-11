793a1835ccc24e42c68af419e18cde7ef97953b0
Sebastian Gallese <sebastiangallese@gmail.com>
2010-01-11 11:54:07 -0500

    pHash Tests

---
+++ src/pHashTest/PHashTest.java
@@ -0,0 +1,9 @@
+package pHashTest;
+
+
+	public class PHashTest {
+//	    public static void main(String[] args) {
+//	    	//ImageHash mypHash = new ImageHash("/home/kdge/Pictures/cc.jpg");
+//
+//	    }
+	}
