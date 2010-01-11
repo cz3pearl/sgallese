f2550bfd6bfc8c71a2f869f7dd8fb096d2527edb
Sebastian Gallese <sebastiangallese@gmail.com>
2010-01-11 12:26:54 -0500

    Test for ImageHash

--- src/pHashTest/PHashTest.java
+++ src/pHashTest/PHashTest.java
@@ -2,8 +2,8 @@ package pHashTest;
 
 
 	public class PHashTest {
-//	    public static void main(String[] args) {
-//	    	//ImageHash mypHash = new ImageHash("/home/kdge/Pictures/cc.jpg");
-//
-//	    }
+	    public static void main(String[] args) {
+	    	//ImageHash mypHash = new ImageHash("/home/kdge/Pictures/cc.jpg");
+
+	    }
 	}
